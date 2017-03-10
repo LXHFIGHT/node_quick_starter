91de79e6249c0fa5d8a765e4efeab46d6696b095
刘栩豪 <liuyh04@mysoft.com.cn>
2017-03-10 19:25:16 +0800

    add some common status code

--- helpers/ResponseHelper.js
+++ helpers/ResponseHelper.js
@@ -53,16 +53,21 @@ module.exports = {
     SUCCESS_LIST_READ: 'fetch list info successfully',
     SUCCESS_DATA_UPDATED: 'data updated successfully',
     SUCCESS_FILE_UPLOADED: 'file uploaded successfully',
+    SUCCESS_ACCESS_TOKEN_GOT: 'wechat access token fetch successfully, it will be expired in 2 hours',
 
     // 警告字符串
     WARNING_NO_TOKEN: 'without token',
+    CODE_WARNING_NO_TOKEN: 401,
     WARNING_INVALID_TOKEN: 'invalid token',
+    CODE_WARNING_INVALID_TOKEN: 402,
     WARNING_EXPIRED_TOKEN: 'expired token',
+    CODE_WARNING_EXPIRED_TOKEN: 403,
     WARNING_NO_DATA_FOUND: 'can not found any data matched',
+    CODE_WARNING_NO_DATA_FOUND: 404,
     WARNING_NO_DATA_DELETE: 'can not found any data matched to delete',
     WARNING_PARAM_REQUIRE_NOT_NULL: 'there are several params requires not null',
 
-    // 错误自渡船
+    // 错误字符串
     ERROR_DATA_INSERTED: 'data insert error',
     ERROR_DATA_UNIQUE: 'data insert error, require unique',
     ERROR_DATA_READ: 'data search error',
